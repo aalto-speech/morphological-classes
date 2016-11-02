3003a20b9a8a96b17daabec31bfe4049e3989822
Matti Varjokallio <matti.varjokallio@iki.fi>
2016-11-02 09:27:15 +0200

    Updated classintppl.

--- classintppl.cc
+++ classintppl.cc
@@ -7,14 +7,14 @@
 #include "defs.hh"
 #include "conf.hh"
 #include "Ngram.hh"
-#include "CatPerplexity.hh"
 
 using namespace std;
 
 
 
 void preprocess_sent(string line,
-                     const Categories &wcs,
+                     const Ngram &lm,
+                     const map<string, pair<int, flt_type> > &class_memberships,
                      string unk_symbol,
                      vector<string> &words,
                      long int &num_words,
@@ -26,7 +26,8 @@ void preprocess_sent(string line,
     while (ss >> word) {
         if (word == "<s>") continue;
         if (word == "</s>") continue;
-        if (wcs.m_class_mem_probs.find(word) == wcs.m_class_mem_probs.end()
+        if (lm.vocabulary_lookup.find(word) == lm.vocabulary_lookup.end()
+            || class_memberships.find(word) == class_memberships.end()
             || word == "<unk>"  || word == "<UNK>")
         {
             words.push_back(unk_symbol);
@@ -44,16 +45,17 @@ void preprocess_sent(string line,
 int main(int argc, char* argv[]) {
 
     conf::Config config;
-    config("usage: classintppl [OPTION...] ARPAFILE CLASS_ARPA WORD_PROBS INPUT\n")
-    ('w', "weight=FLOAT", "arg", "0.5", "Interpolation weight [0.0,1,0] for the word ARPA model")
+    config("usage: classintppl [OPTION...] ARPAFILE CLASS_ARPA CLASS_MEMBERSHIPS INPUT\n")
+    ('i', "weight=FLOAT", "arg", "0.5", "Interpolation weight [0.0,1,0] for the word ARPA model")
     ('r', "use-root-node", "", "", "Pass through root node in contexts with unks, DEFAULT: advance with unk symbol")
+    ('w', "num-words=INT", "arg", "", "Number of words for computing word-normalized perplexity")
     ('h', "help", "", "", "display help");
     config.default_parse(argc, argv);
     if (config.arguments.size() != 4) config.print_help(stderr, 1);
 
     string arpafname = config.arguments[0];
     string classngramfname = config.arguments[1];
-    string wordpfname = config.arguments[2];
+    string classmfname = config.arguments[2];
     string infname = config.arguments[3];
 
     bool root_unk_states = config["use-root-node"].specified;
@@ -86,14 +88,9 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    Categories wcs;
-    cerr << "Reading word probs.." << endl;
-    wcs.read_class_mem_probs(wordpfname);
-    cerr << "Asserting class membership probabilities.." << endl;
-    if (!wcs.assert_class_mem_probs()) {
-        cerr << "Problem in class membership probabilities" << endl;
-        //exit(1);
-    }
+    map<string, pair<int, flt_type> > class_memberships;
+    cerr << "Reading class memberships.." << endl;
+    int num_classes = read_class_memberships(classmfname, class_memberships);
 
     cerr << "Reading class n-gram model.." << endl;
     Ngram class_ng;
@@ -101,11 +98,15 @@ int main(int argc, char* argv[]) {
     int class_lm_start_node = class_ng.advance(class_ng.root_node, class_ng.vocabulary_lookup.at("<s>"));
 
     // The class indexes are stored as strings in the n-gram class
-    vector<int> indexmap(wcs.num_classes());
+    vector<int> indexmap(num_classes);
     for (int i=0; i<(int)indexmap.size(); i++)
         if (class_ng.vocabulary_lookup.find(int2str(i)) != class_ng.vocabulary_lookup.end())
             indexmap[i] = class_ng.vocabulary_lookup[int2str(i)];
         else indexmap[i] = -1;
+    if (indexmap[UNK_CLASS] == -1) {
+        cerr << "Unk class not found in the class n-gram model, forcing use-root-node" << endl;
+        root_unk_states = true;
+    }
 
     cerr << "Scoring sentences.." << endl;
     SimpleFileInput infile(infname);
@@ -124,12 +125,13 @@ int main(int argc, char* argv[]) {
         double sent_ll = 0.0;
 
         vector<string> words;
-        preprocess_sent(line, wcs, unk, words, num_words, num_oovs);
+        preprocess_sent(line, lm, class_memberships, unk, words, num_words, num_oovs);
 
         int curr_class_lm_node = class_lm_start_node;
         int curr_lm_node = lm_start_node;
 
         for (int i=0; i<(int)words.size(); i++) {
+
             if (words[i] == unk) {
                 if (root_unk_states) {
                     curr_lm_node = lm.root_node;
@@ -147,12 +149,11 @@ int main(int argc, char* argv[]) {
             ngram_score *= log(10.0);
             ngram_score += word_iw;
 
-            const CategoryProbs &wcp = wcs.m_class_mem_probs.at(words[i]);
-            assert(wcp.size() == 1);
+            pair<int, flt_type> word_class = class_memberships.at(words[i]);
             double class_score = 0.0;
-            curr_class_lm_node = class_ng.score(curr_class_lm_node, indexmap[wcp.begin()->first], class_score);
+            curr_class_lm_node = class_ng.score(curr_class_lm_node, indexmap[word_class.first], class_score);
             class_score *= log(10.0);
-            class_score += wcp.begin()->second;
+            class_score += word_class.second;
             class_score += class_iw;
 
             sent_ll += add_log_domain_probs(ngram_score, class_score);
@@ -174,15 +175,17 @@ int main(int argc, char* argv[]) {
         num_sents++;
     }
 
-    double ppl = exp(-1.0/double(num_words) * total_ll);
     cerr << endl;
     cerr << "Number of sentences: " << num_sents << endl;
-    //cerr << "Number of skipped sentences: " << num_skipped_sents << endl;
     cerr << "Number of in-vocabulary words excluding sentence ends: " << num_words-num_sents << endl;
     cerr << "Number of in-vocabulary words including sentence ends: " << num_words << endl;
     cerr << "Number of OOV words: " << num_oovs << endl;
     cerr << "Total log likelihood (ln): " << total_ll << endl;
     cerr << "Total log likelihood (log10): " << total_ll/2.302585092994046 << endl;
+
+    if (config["num-words"].specified)
+        num_words = config["num-words"].get_int();
+    double ppl = exp(-1.0/double(num_words) * total_ll);
     cerr << "Perplexity: " << ppl << endl;
 
     exit(0);
