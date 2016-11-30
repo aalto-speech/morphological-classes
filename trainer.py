b85778701469c55750aab7e698cad5619a4e0b4f
Matti Varjokallio <matti.varjokallio@iki.fi>
2016-11-30 12:33:21 +0200

    Working training without tagging.

--- trainer.py
+++ trainer.py
@@ -35,33 +35,70 @@ def init_model(config,
                word_init,
                vocabfname,
                corpus,
-               model_id):
+               init_id):
 
     catem_dir = config.get("common", "catem_dir")
     init_exe = os.path.join(catem_dir, "init")
-    iter0_id = "%s.iter0" % model_id
-    init_cmd = "%s %s %s %s" % (init_exe, word_init, corpus, iter0_id)
-    p = subprocess.Popen(init_cmd, shell=True)
-    p.wait()
+    init_cmd = "%s %s %s %s" % (init_exe, word_init, corpus, init_id)
+    subprocess.Popen(init_cmd, shell=True).wait()
 
     nc_exe = config.get("common", "srilm")
     nc_cmd = "%s -read %s.ccounts.gz -float-counts -unk -wbdiscount -order 1 -vocab %s -lm %s.arpa.gz"\
-                % (nc_exe, iter0_id, vocabfname, iter0_id)
-    p = subprocess.Popen(nc_cmd, shell=True)
-    p.wait()
+                % (nc_exe, init_id, vocabfname, init_id)
+    subprocess.Popen(nc_cmd, shell=True).wait()
+
+
+def catstats(prev_iter_id,
+             curr_iter_id,
+             corpus,
+             single_parse=False):
+
+    catem_dir = config.get("common", "catem_dir")
+    catstats_exe = os.path.join(catem_dir, "catstats")
+
+    stats_cmd = "%s %s.arpa.gz %s.cgenprobs.gz %s.cmemprobs.gz %s %s"\
+                    % (catstats_exe, prev_iter_id, prev_iter_id, prev_iter_id, corpus, curr_iter_id)
+    if single_parse: stats_cmd = "%s -p 1" % stats_cmd
+    subprocess.Popen(stats_cmd, shell=True).wait()
+
+
+def ngram_training(iter_id,
+                   smoothing,
+                   vocab,
+                   order):
+
+    srilm_exe = config.get("common", "srilm")
+    ngram_cmd = "%s -text %s.catseq.gz -unk -vocab %s -order %i -lm %s.arpa.gz" % (srilm_exe, iter_id, vocab, order, iter_id)
+    if smoothing == "wb":
+        ngram_cmd = "%s %s" % (ngram_cmd, "-wbdiscount -interpolate -text-has-weights -float-counts")
+    elif smoothing == "kn":
+        ngram_cmd = "%s %s" % (ngram_cmd, "-kndiscount -interpolate")
+    else:
+        print >>sys.stderr, "Unknown smoothing option"
+        sys.exit(1)
+    print ngram_cmd
+    subprocess.Popen(ngram_cmd, shell=True).wait()
 
 
 def evaluate(model_id,
-             corpus):
+             corpus,
+             resfname=None):
 
     catem_dir = config.get("common", "catem_dir")
     catstats_exe = os.path.join(catem_dir, "catstats")
 
     eval_cmd = "%s %s.arpa.gz %s.cgenprobs.gz %s.cmemprobs.gz %s"\
                     % (catstats_exe, model_id, model_id, model_id, corpus)
-    print eval_cmd
-    p = subprocess.Popen(eval_cmd, shell=True)
-    p.wait()
+
+    if resfname:
+        resf = open(resfname, "a")
+        print >>resf, model_id
+        resf.flush()
+        subprocess.Popen(eval_cmd, stdout=resf, shell=True).communicate()
+        print >>resf, ""
+        resf.close()
+    else:
+        subprocess.Popen(eval_cmd, shell=True).wait()
 
 
 if __name__ == "__main__":
@@ -75,6 +112,8 @@ if __name__ == "__main__":
                         help='Corpus for training the model')
     parser.add_argument('model_id',
                         help='Identifier for the model to be trained')
+    parser.add_argument('--eval_corpus',
+                        help='Corpus for evaluating the model')
     args = parser.parse_args()
 
     config = ConfigParser.ConfigParser()
@@ -85,14 +124,17 @@ if __name__ == "__main__":
     write_vocab(args.word_init, vocab, False)
     write_vocab(args.word_init, vocab_capunk, True)
 
-    #init_model(config, args.word_init, vocab, args.train_corpus, args.model_id)
-    #print >>sys.stderr, "Evaluating training corpus perplexity"
-    #evaluate("%s.iter0" % args.model_id, args.train_corpus)
+    prev_iter_id = "%s.iter0" % args.model_id
+    init_model(config, args.word_init, vocab, args.train_corpus, prev_iter_id)
+    if args.eval_corpus:
+        print >>sys.stderr, "Computing evaluation corpus perplexity"
+        evaluate(prev_iter_id, args.eval_corpus, "%s.eval.ppl" % args.model_id)
 
     iterations = config.items("training")
     for iteration in iterations:
+        iter_id = "%s.%s" % (args.model_id, iteration[0])
         print >>sys.stderr, ""
-        print >>sys.stderr, "Training %s" % iteration[0]
+        print >>sys.stderr, "Training %s" % iter_id
         smoothing, order, update_catprobs, tag = iteration[1].split(",")
         order = int(order)
         update_catprobs = update_catprobs in ["true", "True", "1"]
@@ -101,3 +143,11 @@ if __name__ == "__main__":
         print >>sys.stderr, "Update category probabilities: %s" % update_catprobs
         print >>sys.stderr, "Tag unks: %s" % tag
 
+        catstats(prev_iter_id, iter_id, args.train_corpus, smoothing=="kn")
+        ngram_training(iter_id, smoothing, vocab, order)
+
+        if args.eval_corpus:
+            print >>sys.stderr, "Computing evaluation corpus perplexity"
+            evaluate(iter_id, args.eval_corpus, "%s.eval.ppl" % args.model_id)
+
+        prev_iter_id = iter_id
