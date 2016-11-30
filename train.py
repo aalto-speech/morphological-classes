e5dfa1e144f6aee445d26b984964867ecc633a82
Matti Varjokallio <matti.varjokallio@iki.fi>
2016-11-30 08:58:28 +0200

    Argparse and +x.

--- train.py
+++ train.py
@@ -2,17 +2,18 @@
 
 import os
 import sys
+import argparse
 import subprocess
 
 
 def write_vocab(initfname,
                 vocabfname="vocab",
-                varikn=False):
+                uppercase_unk=False):
 
     vocabf = open(vocabfname, "w")
     print >>vocabf, "<s>"
     print >>vocabf, "</s>"
-    if varikn:
+    if uppercase_unk:
         print >>vocabf, "<UNK>"
     else:
         print >>vocabf, "<unk>"
@@ -24,11 +25,19 @@ def write_vocab(initfname,
         tokens = line.split()
         for token in tokens[1:]:
             categories.add(token)
-    for cat in categories:
+    for cat in sorted_categories:
         print >>vocabf, cat
     vocabf.close()
 
 
 if __name__ == "__main__":
 
-    write_vocab("words.init", "vocab", False)
+    parser = argparse.ArgumentParser(description='Training script for a category n-gram model .')
+    parser.add_argument('word_init',
+                        help='Initial categories for the words', )
+    parser.add_argument('--varikn', action='store_true',
+                        help='Use VariKN for training the n-gram component')
+    args = parser.parse_args()
+
+    write_vocab(args.word_init, "vocab", args.varikn)
+
