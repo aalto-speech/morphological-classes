73f14ec774b1e926fc1d4cd7b85d0e17b258cf6b
Matti Varjokallio <matti.varjokallio@aalto.fi>
2019-06-03 09:14:00 +0300

    Started updating stats script for the latest Omorfi.

--- omorfi/omorfi_parse.py
+++ omorfi/omorfi_parse.py
@@ -1,43 +1,37 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
+import argparse
+import codecs
+import locale
 import re
 import sys
-import locale
-import codecs
 
+
+excluded_fields = ["BLACKLIST", "COMPOUND_WORD_ID"]
 
 def parse_analyses(fname, encoding="utf8"):
 
-    analf = codecs.open(fname, encoding=encoding)
+    analysisFile = codecs.open(fname, encoding=encoding)
     analyses = dict()
 
-    for line in analf:
+    for line in analysisFile:
         line = line.strip()
         if not len(line): continue
         if line.startswith("Strings\tFound"): break
 
         tokens = line.split("\t")
-        wrd = tokens[0]
-        analysis_tokens = tokens[1].split()
+        if len(tokens) != 2:
+            raise Exception("Erroneous Omorfi analysis: %s" % line)
 
-        tmpi = 0
-        for i in range(len(analysis_tokens)):
-            if r"#" in analysis_tokens[i]: tmpi = i
-        if tmpi > 0: analysis_tokens = [analysis_tokens[0]] + analysis_tokens[tmpi+1:]
+        word = tokens[0]
+        if not word in analyses: analyses[word] = set()
 
-        if len(analysis_tokens) > 2:
-            toks = "_".join(set(analysis_tokens[2:]))
-            wrd_analysis = "%s_%s" % (analysis_tokens[1], toks)
-        elif len(analysis_tokens) > 1:
-            wrd_analysis = "%s" % analysis_tokens[1]
-        else:
-            wrd_analysis = ""
-
-        lowercase = analysis_tokens[0].lower() == analysis_tokens[0]
-        if lowercase: wrd = wrd.lower()
-
-        if not wrd in analyses: analyses[wrd] = set()
-        if len(wrd_analysis): analyses[wrd].add(wrd_analysis)
+        analysis_tokens = re.findall("\[(.*?)\]", tokens[1])
+        if analysis_tokens:
+            analysis_tokens = analysis_tokens[1:]
+            analysis_tokens = list(filter(lambda x: x.split("=")[0] not in excluded_fields, analysis_tokens))
+            word_analysis = "_".join(analysis_tokens)
+            if len(word_analysis): analyses[word].add(word_analysis)
 
     return analyses
 
@@ -50,16 +44,22 @@ def analysis_counts(analyses):
     words_w_lc_analysis = set()
 
     for word, word_analyses in analyses.items():
-
-        lc_word = word.lower()
-        lowercase = lc_word == word
-
         if len(word_analyses):
-            words_w_analyses.add(lc_word)
-            if lowercase: words_w_lc_analysis.add(lc_word)
-            else: words_w_uc_analysis.add(lc_word)
+            words_w_analyses.add(word)
+
+            uc_analysis_found = False
+            lc_analysis_found = False
+            for word_analysis in word_analyses:
+                if "UPOS=PROPN" in word_analysis:
+                    uc_analysis_found = True
+                else:
+                    lc_analysis_found = True
+            if uc_analysis_found:
+                words_w_uc_analysis.add(word)
+            if lc_analysis_found:
+                words_w_lc_analysis.add(word)
         else:
-            words_wo_analyses.add(lc_word)
+            words_wo_analyses.add(word)
 
     num_uc = len(words_w_uc_analysis)
     num_lc = len(words_w_lc_analysis)
@@ -68,31 +68,16 @@ def analysis_counts(analyses):
     return len(words_w_analyses), num_uc, num_lc, num_both
 
 
-def merge_uc_lc(analyses):
-
-    uc_words = set()
-    for word, word_analyses in analyses.items():
-        lc_word = word.lower()
-        if word == lc_word: continue
-        if not lc_word in analyses: analyses[lc_word] = set()
-        analyses[lc_word] |= analyses[word]
-        uc_words.add(word)
-    for uc_word in uc_words:
-        del analyses[uc_word]
-
-    return analyses
-
-
 def get_num_analyses_per_word(analyses):
-    anal_counts = dict()
-    for word in analyses.keys():
-        if not analyses[word]: continue
-        anal_count = len(analyses[word])
-        if not anal_count in anal_counts:
-            anal_counts[anal_count] = 0
-        anal_counts[anal_count] += 1
+    analysis_counts = dict()
+    for word, word_analyses in analyses.items():
+        if not word_analyses: continue
+        word_analysis_count = len(word_analyses)
+        if not word_analysis_count in analysis_counts:
+            analysis_counts[word_analysis_count] = 0
+        analysis_counts[word_analysis_count] += 1
 
-    return anal_counts
+    return analysis_counts
 
 
 def get_analysis_classes(analyses):
@@ -109,31 +94,29 @@ def get_analysis_classes(analyses):
 
 if __name__ == "__main__":
 
-    if len(sys.argv) < 2:
-        print >>sys.stderr, "USAGE: ANALYSIS"
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="Prints statistics about the Omorfi analyses")
+    parser.add_argument("ANALYSES", help="Output from the Omorfi analyzer.")
+    args = parser.parse_args()
 
     locale.setlocale(locale.LC_ALL, 'en_US.utf8')
-    analyses = parse_analyses(sys.argv[1])
+    analyses = parse_analyses(args.ANALYSES)
 
     num_w_analyses, num_uc, num_lc, num_both = analysis_counts(analyses)
-    analyses = merge_uc_lc(analyses)
 
-    print >>sys.stderr, "Number of words: %i" % len(analyses)
-    print >>sys.stderr, "Words with analysis: %i" % num_w_analyses
-    print >>sys.stderr, "Words with uppercase analysis: %i" % num_uc
-    print >>sys.stderr, "Words with lowercase analysis: %i" % num_lc
-    print >>sys.stderr, "Words with both upper and lowercase analysis: %i" % num_both
+    print("Number of words: %i" % len(analyses), file=sys.stderr)
+    print("Words with analysis: %i" % num_w_analyses, file=sys.stderr)
+    print("Words with uppercase analysis: %i" % num_uc, file=sys.stderr)
+    print("Words with lowercase analysis: %i" % num_lc, file=sys.stderr)
+    print("Words with both upper and lowercase analysis: %i" % num_both, file=sys.stderr)
 
-    print >>sys.stderr, ""
+    print("", file=sys.stderr)
     counts = get_num_analyses_per_word(analyses)
-    for analc, c in counts.items():
-        print >>sys.stderr, "Words with %i analyses: %i" % (analc, c)
+    for analysis_count, word_count in counts.items():
+        print("Words with %i analyses: %i" % (analysis_count, word_count), file=sys.stderr)
 
-    print >>sys.stderr, ""
-    anal_types = get_analysis_classes(analyses)
-    print >>sys.stderr, "Distinct morphological classes: %i" % len(anal_types)
+    print("", file=sys.stderr)
+    analysis_types = get_analysis_classes(analyses)
+    print("Distinct morphological classes: %i" % len(analysis_types), file=sys.stderr)
 
-#    for anal_type, count in anal_types.items():
-#        print "%s\t%i" % (anal_type, count)
-
+#    for analysis_type, count in analysis_types.items():
+#        print("%s\t%i" % (analysis_type, count))
