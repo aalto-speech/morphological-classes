2766c817eba766195a446533b47e272c686851aa
Matti Varjokallio <matti.varjokallio@aalto.fi>
2019-06-04 10:11:02 +0300

    Refactored init script.

--- omorfi/omorfi_init_words.py
+++ omorfi/omorfi_init_words.py
@@ -8,32 +8,13 @@ from omorfi_parse import read_analyses
 from omorfi_parse import get_analysis_classes
 
 
-def state_with_analyses(word_analyses, clsmap):
-    classes = set()
-    for analysis in word_analyses:
-        classes.add(clsmap[analysis])
-    return classes
-
-
-def initialize(analyses, clsmap):
-    word_states = dict()
-    for word, word_analyses in analyses.items():
-        if word_analyses:
-            word_states[word] = state_with_analyses(word_analyses, clsmap)
-        # in-vocabulary word with no analyses
-        else:
-            word_states[word] = set()
-
-    return word_states
-
-
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Initialize word categories with Omorfi analyses')
     parser.add_argument("ANALYSES", help="Output from the Omorfi analyzer without the -X switch.")
     parser.add_argument("LARGE_COVERAGE_ANALYSES", help="Output from the Omorfi analyzer with the -X switch.")
     parser.add_argument('WORD_INIT', help='Word initializations to be written')
-    parser.add_argument('CLASS_DEFS', help='Class information to be written')
+    parser.add_argument('CLASS_DEFS', help='Class definitions to be written')
     parser.add_argument("--encoding", action="store", type=str, default="utf-8")
     args = parser.parse_args()
 
@@ -46,11 +27,10 @@ if __name__ == "__main__":
     rev_clsmap = dict(enumerate(sorted(classes.keys())))
     clsmap = dict(map(reversed, enumerate(sorted(classes.keys()))))
 
-    word_state_vectors = initialize(analyses, clsmap)
-
     wordf = codecs.open(args.WORD_INIT, "w", encoding=args.encoding)
-    for word, vector in word_state_vectors.items():
-        print("%s\t%s" % (word, " ".join(map(str, vector))), file=wordf)
+    for word, word_analyses in sorted(analyses.items()):
+        word_class_idxs = map(lambda x: clsmap[x], word_analyses)
+        print("%s\t%s" % (word, " ".join(map(str, word_class_idxs))), file=wordf)
     wordf.close()
 
     classf = open(args.CLASS_DEFS, "w")
