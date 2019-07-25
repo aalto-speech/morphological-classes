717f70fddb446ecb2d3ae6d99845f74655349fa1
Matti Varjokallio <matti.varjokallio@iki.fi>
2019-07-25 13:47:09 +0300

    Option to write interpolated probabilities to file.

--- scripts/interpolate.py
+++ scripts/interpolate.py
@@ -8,11 +8,15 @@ import argparse
 from operator import itemgetter
 
 
-def openFile(fname):
+unkSymbol = "<unk>"
+smallLP = -1000.0
+
+
+def openFile(fname, mode="r"):
     if fname.endswith(".gz"):
-        return gzip.open(fname, "rt")
+        return gzip.open(fname, mode+"t")
     else:
-        return open(fname, "r")
+        return open(fname, mode)
 
 
 def add_log_domain_probs(a, b):
@@ -22,42 +26,8 @@ def add_log_domain_probs(a, b):
         delta = -delta
     return b + math.log1p(math.exp(delta))
 
-unkSymbol = "<unk>"
-smallLP = -1000.0
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Interpolates log likelihoods in two probability files.')
-    parser.add_argument('prob_file_1', action="store",
-                        help='The first probability file written by a ppl executable')
-    parser.add_argument('prob_file_2', action="store",
-                        help='The second probability file written by a ppl executable')
-    parser.add_argument('-w', '--weight', action="store", type=float,
-                        help='Interpolation weight for the first model in the range [0.0,1.0]')
-    parser.add_argument('-o', '--optimize_weights', action="store_true", default=False,
-                        help='Finds the best interpolation weight in steps of 0.05')
-    parser.add_argument('-n', '--num_words', action="store", type=int,
-                        help='Number of words for computing word-normalized perplexity')
-    parser.add_argument('-u', '--allow_unks', action="store_true", default=False,
-                        help='Score words included only in the other model, probability assigned by the other model will be 0.0')
-    args = parser.parse_args()
-
-    totalLLs = dict()
-    if args.weight:
-        if args.weight < 0.0 or args.weight > 1.0:
-            raise Exception("Invalid weight %f" % args.weight)
-        first_log_iw = math.log(args.weight)
-        second_log_iw = math.log(1.0 - args.weight)
-        totalLLs[(first_log_iw, second_log_iw)] = 0.0
-    elif args.optimize_weights:
-        iws = [x * 0.05 for x in range(1,20)]
-        for iw in iws:
-            first_log_iw = math.log(iw)
-            second_log_iw = math.log(1.0 - iw)
-            totalLLs[(first_log_iw, second_log_iw)] = 0.0
-    else:
-        raise Exception("define either --weight or --optimize_weights")
-
+def compute_likelihood(args):
     probf1 = openFile(args.prob_file_1)
     probf2 = openFile(args.prob_file_2)
 
@@ -88,6 +58,7 @@ if __name__ == '__main__':
                 for weights in totalLLs.keys():
                     interpolated_prob = add_log_domain_probs(weights[0] + score1, weights[1] + score2)
                     totalLLs[weights] += interpolated_prob
+
     best_weights, totalLL = max(totalLLs.items(), key=itemgetter(1))
 
     print("", file=sys.stderr)
@@ -107,3 +78,76 @@ if __name__ == '__main__':
         wnppl = math.exp(-1.0 / float(args.num_words) * totalLL)
         print("Word-normalized perplexity: %f" % wnppl, file=sys.stderr)
 
+    return best_weights
+
+
+def write_probs(args, best_weights):
+    probf1 = openFile(args.prob_file_1)
+    probf2 = openFile(args.prob_file_2)
+    outprobf = openFile(args.prob_file, "w")
+
+    lineIdx = 0
+    for probs1 in probf1:
+        lineIdx += 1
+        probs1 = probs1.strip().split()
+        probs2 = probf2.readline().strip().split()
+
+        if len(probs1) != len(probs2):
+            raise Exception("Files do not match on line %i" % lineIdx)
+
+        if not len(probs1): continue
+
+        sentence = []
+        for i in range(len(probs1)):
+            if probs1[i] == unkSymbol and probs2[i] == unkSymbol:
+                sentence.append(unkSymbol)
+            elif not args.allow_unks and probs1[i] == unkSymbol:
+                sentence.append(unkSymbol)
+            elif not args.allow_unks and probs2[i] == unkSymbol:
+                sentence.append(unkSymbol)
+            else:
+                score1 = float(probs1[i]) if probs1[i] != unkSymbol else smallLP
+                score2 = float(probs2[i]) if probs2[i] != unkSymbol else smallLP
+                interpolated_prob = add_log_domain_probs(best_weights[0] + score1, best_weights[1] + score2)
+                sentence.append('{:.4e}'.format(interpolated_prob))
+        print(" ".join(sentence), file=outprobf)
+    outprobf.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Interpolates log likelihoods in two probability files.')
+    parser.add_argument('prob_file_1', action="store",
+                        help='The first probability file written by a ppl executable')
+    parser.add_argument('prob_file_2', action="store",
+                        help='The second probability file written by a ppl executable')
+    parser.add_argument('-w', '--weight', action="store", type=float,
+                        help='Interpolation weight for the first model in the range [0.0,1.0]')
+    parser.add_argument('-o', '--optimize_weights', action="store_true", default=False,
+                        help='Finds the best interpolation weight in steps of 0.05')
+    parser.add_argument('-n', '--num_words', action="store", type=int,
+                        help='Number of words for computing word-normalized perplexity')
+    parser.add_argument('-u', '--allow_unks', action="store_true", default=False,
+                        help='Score words included only in the other model, probability assigned by the other model will be 0.0')
+    parser.add_argument('-f', '--prob_file', action="store",
+                        help='Write interpolated log likelihoods (ln) to a file')
+    args = parser.parse_args()
+
+    totalLLs = dict()
+    if args.weight:
+        if args.weight < 0.0 or args.weight > 1.0:
+            raise Exception("Invalid weight %f" % args.weight)
+        first_log_iw = math.log(args.weight)
+        second_log_iw = math.log(1.0 - args.weight)
+        totalLLs[(first_log_iw, second_log_iw)] = 0.0
+    elif args.optimize_weights:
+        iws = [x * 0.05 for x in range(1,20)]
+        for iw in iws:
+            first_log_iw = math.log(iw)
+            second_log_iw = math.log(1.0 - iw)
+            totalLLs[(first_log_iw, second_log_iw)] = 0.0
+    else:
+        raise Exception("define either --weight or --optimize_weights")
+
+    best_weights = compute_likelihood(args)
+    if args.prob_file:
+        write_probs(args, best_weights)
