228845e2232fe472aa5ea36e58ca68107f70913e
Matti Varjokallio <matti.varjokallio@iki.fi>
2019-08-08 23:12:47 +0300

    Interpolation works with defined weights.

--- scripts/interpolate2.py
+++ scripts/interpolate2.py
@@ -54,7 +54,11 @@ def read_probs(prob_fnames, allow_unks=False):
             for s in range(len(probs)):
                 if probs[s][i] == unkSymbol:
                     unkFound = True
-                elif probs[s][i].isnumeric():
+                else:
+                    try:
+                        val = float(probs[s][i])
+                    except:
+                        raise Exception("Unexpected token: %s" % probs[s][i])
                     probFound = True
 
             if not allow_unks and unkFound:
@@ -71,11 +75,38 @@ def read_probs(prob_fnames, allow_unks=False):
     return allProbs, numUnks, numSents
 
 
+def find_optimal_weights(allProbs):
+    pass
+
+
+def compute_likelihood(allProbs, weights):
+    totalLL = 0.0
+    for probs in allProbs:
+        currLL = weights[0] + probs[0]
+        for i in range(1,len(weights)):
+            currLL = add_log_domain_probs(currLL, weights[i] + probs[i])
+        totalLL += currLL
+    return totalLL
+
+
+def print_result(numSents, numUnks, numWords, best_weights, totalLL):
+    print("Number of sentences: %i" % numSents, file=sys.stderr)
+    print("Number of in-vocabulary words exluding sentence ends: %i" % (numWords - numSents), file=sys.stderr)
+    print("Number of in-vocabulary words including sentence ends: %i" % numWords, file=sys.stderr)
+    print("Number of OOV words: %i" % numUnks, file=sys.stderr)
+    print("OOV rate: %f %%" % (100.0 * float(numUnks) / float(numUnks + numWords - numSents)), file=sys.stderr)
+    print("Interpolation weights: %f, %f" % (math.exp(best_weights[0]), math.exp(best_weights[1])), file=sys.stderr)
+    print("Total log likelihood (ln): %s" % ('{:.5e}'.format(totalLL)), file=sys.stderr)
+    print("Total log likelihood (log10): %s" % ('{:.5e}'.format(totalLL / 2.302585092994046)), file=sys.stderr)
+    ppl = math.exp(-1.0 / float(numWords) * totalLL)
+    print("Perplexity: %.2f" % ppl, file=sys.stderr)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Interpolates log likelihoods in two probability files.')
-    parser.add_argument('input_prob_file', nargs='+', help='Probability files written by a ppl executable')
-    parser.add_argument('-w', '--weight', action="store", type=float,
-                        help='Interpolation weight for the first model in the range [0.0,1.0]')
+    parser.add_argument('input_prob_files', nargs='+', help='Probability files written by a ppl executable')
+    parser.add_argument('-w', '--weights', action="store",
+                        help='Interpolation weight for the models in the range, e.g. 0.6,0.4')
     parser.add_argument('-o', '--optimize_weights', action="store_true", default=False,
                         help='Finds the best interpolation weight in steps of 0.05')
     parser.add_argument('-n', '--num_words', action="store", type=int,
@@ -86,8 +117,20 @@ if __name__ == '__main__':
                         help='Write interpolated log likelihoods (ln) to a file')
     args = parser.parse_args()
 
-    print(args.input_prob_file)
-    allProbs, numUnks, numSents = read_probs(args.input_prob_file)
-    print(len(allProbs))
-    print(numUnks)
-    print(numSents)
+    if args.weights:
+        weights = list(map(lambda x: float(x), args.weights.split(",")))
+        if len(weights) != len(args.input_prob_files):
+            raise Exception("Wrong number of interpolation weights: %s" % args.weights)
+        if abs(sum(weights) - 1.0) > 0.00001:
+            raise Exception("Interpolation weights do not sum to 1, total was: %f" % sum(weights))
+        weights = list(map(lambda x: math.log(x), weights))
+        allProbs, numUnks, numSents = read_probs(args.input_prob_files)
+        print(weights)
+        totalLL = compute_likelihood(allProbs, weights)
+        print_result(numSents, numUnks, len(allProbs), weights, totalLL)
+    elif args.optimize_weights:
+        allProbs, numUnks, numSents = read_probs(args.input_prob_files)
+        weights, totalLL = find_optimal_weights(allProbs)
+        print_result(numSents, numUnks, len(allProbs), weights, totalLL)
+    else:
+        raise Exception("Define either --weights or --optimize_weights")
